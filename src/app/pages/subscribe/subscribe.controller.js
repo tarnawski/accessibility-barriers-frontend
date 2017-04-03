d5a18b83a6c3ad45ab75fe119c6e5920b982ddc8
Tomasz <tarnawski27@gmail.com>
2017-04-03 14:56:06 +0200

    Small changes in subscription

--- src/app/pages/subscribe/subscribe.controller.js
+++ src/app/pages/subscribe/subscribe.controller.js
@@ -9,23 +9,24 @@
   function SubscribeController(communicationFactory, $stateParams) {
 
       var vm = this;
+      vm.isUnSubscribe = false;
+      vm.unSubscribe = unSubscribe;
 
       activate();
 
       ///////////////
 
       function activate() {
-          var secret = $stateParams.secret;
-          unSubscribe(secret);
+          vm.secret = $stateParams.secret;
       }
 
-      function unSubscribe(secret) {
-          communicationFactory.subscribers.delete({secret: secret},
+      function unSubscribe() {
+          communicationFactory.subscribers.delete({secret: vm.secret},
               function () {
                   vm.isUnSubscribe = true;
               },
               function () {
-                  vm.isUnSubscribe = false;
+                  vm.isUnSubscribe = true;
               }
           );
       }
